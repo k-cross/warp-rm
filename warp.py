954a68f531a332e754cc5fd2b0f630a2b3a00310
ken <kenneth.cross@sjsu.edu>
2016-09-20 22:24:29 -0700

    create mapper completed with tests

--- warp.py
+++ warp.py
@@ -7,8 +7,7 @@ import re
 <Desc.>  This function creates the mapping of files where the key is the name before a
          period and the value is a list of extensions with the same key.
 
-TODO:
-<Issues> What are the corner cases?
+TODO: <Issues> What are the corner cases?
          1) File starts with a .
          2) The compare extensions contains multiple .s
          3) There are no .s in the file
@@ -20,12 +19,40 @@ def create_map(directory):
     for i in range(0, len(dir_list)):
         kv_pair = re.split(r'\.', dir_list[i])
 
-        if len(kv_pair) > 2:
-            pass
+        # Always create a list
+        if len(kv_pair) == 2:
+            if kv_pair[0] == '':
+                if None not in file_map:
+                    file_map[None] = []
+                file_map[None].append(kv_pair[1])
+            else:
+                if kv_pair[0] not in file_map:
+                    file_map[kv_pair[0]] = []
+                file_map[kv_pair[0]].append(kv_pair[1])
+        #TODO: Still Unhandled
+        elif len(kv_pair) > 2:
+            s = kv_pair[0] + '.'
+
+            if kv_pair[0] == '':
+                s = '.'
+
+            for j in range(1, len(kv_pair) - 1):
+                s += kv_pair[j] + '.'
+
+            if s[0:-1] not in file_map:
+                file_map[s[0:-1]] = []
+
+            file_map[s[0:-1]].append(kv_pair[len(kv_pair) - 1])
+        elif len(kv_pair) == 1 and kv_pair[0] != '':
+            if kv_pair[0] not in file_map:
+                file_map[kv_pair[0]] = []
+            file_map[kv_pair[0]].append(None)
         else:
             pass
 
-def remove_duplicates(cmp_ext, rm_ext, directory):
+    return file_map
+
+def remove_nonduplicates(cmp_ext, rm_ext, directory):
     file_map = create_map(cmp_ext, rm_ext, directory)
 
     #TODO: Modify this for correctness and features later
