cd2de9eff7c44d5bd8b6f3dc57feee7da9d40851
Kenneth Cross <ken.q.cross@gmail.com>
2021-05-07 15:39:55 -0700

    traverse a file path and store files in associated map

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 extern crate clap;
-extern crate glob;
 
-use clap::{App, Arg};
-use glob::glob;
+use clap::{App, Arg, ArgMatches};
+use std::collections::HashMap;
+use std::path::{Path, PathBuf};
 
 fn main() {
     // Dear reader, if you can make this prettier, I'd like to hear about it.
@@ -29,7 +29,15 @@ then any `raw`, `csv`, `jpg`, `gif`, or `exe` will be deleted.
     let matches = App::new("Warp-Utilities Remove")
         .version("0.1.0")
         .about("Warp file remover removes files with unmatching extensions")
-        .author("Ken Cross")
+        // positional
+        .arg(
+            Arg::with_name("target_paths")
+                // TODO: add multiple later
+                .multiple(false)
+                .required(true)
+                .help("Specify the directory paths to scan"),
+        )
+        // options
         .arg(
             Arg::with_name("target_ext")
                 .short("t")
@@ -47,6 +55,13 @@ then any `raw`, `csv`, `jpg`, `gif`, or `exe` will be deleted.
                 .required(true)
                 .takes_value(true)
                 .help("Specify extensions in which existence is required"),
+        )
+        // flags
+        .arg(
+            Arg::with_name("recursive")
+                .short("r")
+                .long("recursive")
+                .help("Scans directories recursively"),
         )
         .arg(
             Arg::with_name("case_sensitive")
@@ -74,24 +89,97 @@ then any `raw`, `csv`, `jpg`, `gif`, or `exe` will be deleted.
         )
         .get_matches();
 
-    if let Some(targets) = matches.values_of("target_ext") {
+    let file_map = build_file_map(matches);
+}
+
+fn build_file_map(matches: ArgMatches) -> HashMap<String, Vec<String>> {
+    if let (Some(targets), Some(sources)) = (
+        matches.values_of("target_ext"),
+        matches.values_of("source_ext"),
+    ) {
         for m in targets {
             println!("targets {}\n", m);
         }
-    }
-
-    if let Some(sources) = matches.values_of("source_ext") {
         for m in sources {
             println!("sources {}\n", m);
         }
     }
 
-    //for entry in glob("**/*").expect("Failed to read glob pattern") {
-    //    match entry {
-    //        Ok(path) => println!("{:?}", path.display()),
-    //        Err(e) => println!("Failed to read file: {:?}", e),
-    //    }
-    //}
+    // TODO: add multiple target paths later
+    let target_path = Path::new(matches.value_of("target_paths").unwrap());
+    let file_list = get_files(target_path, matches.is_present("recursive"));
+    let mut file_table: HashMap<String, Vec<String>> = HashMap::new();
+
+    for file in file_list {
+        match file.parent() {
+            Some(parent) => {
+                let stem = file.file_stem().unwrap().to_str().unwrap();
+                construct_key_value(&file, &stem, &parent.to_str().unwrap(), &mut file_table);
+            }
+            _ => {
+                let stem = file.file_stem().unwrap().to_str().unwrap();
+                construct_key_value(&file, &stem, &"", &mut file_table);
+            }
+        }
+    }
+
+    println!("File Table: {:?}", file_table);
+    file_table
 }
 
-pub fn build_glob_pattern() {}
+fn construct_key_value<'a>(
+    file: &'a Path,
+    stem: &'a str,
+    parent: &'a str,
+    file_table: &'a mut HashMap<String, Vec<String>>,
+) -> &'a mut HashMap<String, Vec<String>> {
+    // clean up to just referenece String directly
+    let mut path_stem = String::from(parent);
+    path_stem.push('/');
+    path_stem.push_str(stem);
+
+    if let Some(extension) = file.extension() {
+        insert_file_table(file_table, path_stem, &extension.to_str().unwrap())
+    } else {
+        insert_file_table(file_table, path_stem, &"")
+    }
+}
+
+fn insert_file_table<'a>(
+    file_table: &'a mut HashMap<String, Vec<String>>,
+    path: String,
+    ext: &'a str,
+) -> &'a mut HashMap<String, Vec<String>> {
+    let exts = file_table.entry(path).or_insert(Vec::new());
+    exts.push(String::from(ext));
+
+    file_table
+}
+
+fn get_files<'a>(p: &'a Path, recurse: bool) -> Vec<PathBuf> {
+    let mut file_list: Vec<PathBuf> = Vec::new();
+
+    for file in p.read_dir().expect("Error reading directory") {
+        match file {
+            Ok(fp) => {
+                let fp = fp.path();
+
+                if fp.is_file() {
+                    file_list.push(fp.to_path_buf())
+                } else if recurse && fp.is_dir() {
+                    let mut temp_list = get_files(&fp, recurse);
+                    file_list.append(&mut temp_list)
+                }
+            }
+            Err(e) => println!("Failed to read file: {:?}", e),
+        }
+    }
+
+    file_list
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn basics() {}
+}
